1c5f4dcb5d794ece3257c6b74dbf0474da06eaa4
rafael <rafael.apedro95@gmail.com>
2022-12-31 20:18:38 -0300

    Criação do metodo de listagem e deleção

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,80 @@
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+
+    public static final Integer SAIR = 4;
+    public static void main(String[] args){
 
         ArrayList<Produto> todosOsProdutos = new ArrayList<>();
 
         Scanner ler = new Scanner(System.in);
-        Integer cod = 1;
+        Integer proxCodBarrasProduto = 1;
         Integer opcao = 1;
 
-        while(opcao == 1) {
+
+        while(opcao != SAIR) {
             System.out.println("Selecione uma opção: ");
             System.out.println("1- Cadastrar Produto");
-            System.out.println("2- Breve");
+            System.out.println("2- Listar produtos");
+            System.out.println("3- Deletar produto");
+            System.out.println("4- Sair");
             opcao = ler.nextInt();
             switch (opcao) {
-                case 1:
-                    System.out.println("Criação de novo Produto");
-                    Produto produto = new Produto();
-                    produto.setCodigo(cod);
-                    cod += 1;
-
-                    System.out.println("Digite a descrição do Produto: ");
-                    produto.setDescricao(ler.next());
-
-
-                    System.out.println("Digite o preço do produto: ");
-                    String gambiarra = ler.next();
-                    produto.setPreco(Double.parseDouble(gambiarra));
-
-                    System.out.println("Digite a quantidade a ser cadastrada no estoque: ");
-                    produto.setEstoque(ler.nextInt());
-                    produto.baixaEstoque(1);
-
-                    todosOsProdutos.add(produto);
-                    break;
-                case 2:
-                    System.out.println("Nada ainda");
-                    break;
-                default:
-                    System.out.println("Opção invalida");
+                case 1 -> {
+                    cadastroDeProdutos(proxCodBarrasProduto, todosOsProdutos);
+                    proxCodBarrasProduto += 1;
+                }
+                case 2 -> listagemDeProdutos(todosOsProdutos);
+                case 3 -> deletarProduto(todosOsProdutos);
+                case 4 -> System.out.println("Obrigado por utilizar o SisPetro");
+                default -> System.out.println("Opção invalida");
             }
+
         }
+    }
+    public static void cadastroDeProdutos(Integer proxCodBarrasProduto, ArrayList<Produto> todosOsProdutos){
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Criação de novo Produto");
+        Produto produto = new Produto();
+        produto.setCodigo(proxCodBarrasProduto);
+
+        System.out.println("Digite a descrição do Produto: ");
+        produto.setDescricao(ler.next());
+
+
+        System.out.println("Digite o preço do produto: ");
+        String gambiarra = ler.next();
+        produto.setPreco(Double.parseDouble(gambiarra));
+
+        System.out.println("Digite a quantidade a ser cadastrada no estoque: ");
+        produto.setEstoque(ler.nextInt());
+        //produto.baixaEstoque(1);
+
+        todosOsProdutos.add(produto);
+    }
+
+    public static void listagemDeProdutos(ArrayList<Produto> todosOsProdutos){
+        /*for(int x=0; x < todosOsProdutos.size(); x++){
+            System.out.print(todosOsProdutos.get(x).getCodigo() + "\t");
+            System.out.print(todosOsProdutos.get(x).getDescricao()+ "\t");
+            System.out.print(todosOsProdutos.get(x).getPreco()+ "\t");
+            System.out.println(todosOsProdutos.get(x).getEstoque()+ "\t");
+        }*/
+        for(Produto p : todosOsProdutos){
+            System.out.print(p.getCodigo() + "\t");
+            System.out.print(p.getDescricao()+ "\t");
+            System.out.print(p.getPreco()+ "\t");
+            System.out.println(p.getEstoque()+ "\t");
+        }
+    }
+
+    public static void deletarProduto(ArrayList<Produto> todosOsProdutos){
+        Scanner ler = new Scanner(System.in);
+        listagemDeProdutos(todosOsProdutos);
+        System.out.println("Digite o código do produto que deseja deletar: ");
+        Integer codigoADeletar = ler.nextInt();
+        todosOsProdutos.removeIf( produto -> produto.getCodigo() == codigoADeletar);
     }
 }
--- src/Produto.java
+++ src/Produto.java
@@ -2,25 +2,42 @@ public class Produto {
        private Integer codigo;
        private String descricao;
        private Double preco;
-       private Integer quantideEstoque;
+       private Integer quantidadeEstoque;
 
-       public void setCodigo(Integer cod){
-              this.codigo = cod;
+       public void setCodigo(Integer codigo){
+              this.codigo = codigo;
        }
 
-       public void setDescricao(String descri){
-              this.descricao = descri.toUpperCase();
+       public void setDescricao(String descricao){
+              this.descricao = descricao.toUpperCase();
        }
 
-       public void setPreco(Double prec){
-              this.preco = prec;
+       public void setPreco(Double preco){
+              this.preco = preco;
        }
 
-       public void setEstoque(Integer quantEstoque){
-              this.quantideEstoque = quantEstoque;
+       public void setEstoque(Integer quantidadeEstoque){
+              this.quantidadeEstoque = quantidadeEstoque;
        }
 
-       public void baixaEstoque(Integer quantVendida){
-              this.quantideEstoque -= quantVendida;
+       public void baixaEstoque(Integer quantidadeVendida){
+              this.quantidadeEstoque -= quantidadeVendida;
+       }
+
+
+       public Integer getCodigo(){
+              return this.codigo;
+       }
+
+       public String getDescricao(){
+              return this.descricao;
+       }
+
+       public Double getPreco(){
+              return this.preco;
+       }
+
+       public Integer getEstoque(){
+              return this.quantidadeEstoque;
        }
 }

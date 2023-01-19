690068d5fad9418d7f5d337fcf4908546781bd3f
rafael <rafael.apedro95@gmail.com>
2023-01-18 21:16:12 -0300

    criação de teste unitário de venda

--- src/test/java/VendaTest.java
+++ src/test/java/VendaTest.java
@@ -2,6 +2,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class VendaTest {
+
+
     @Test
     public void deveSomarOValorDosProdutosVendidos(){
         Produto produto = new Produto();
@@ -10,6 +12,22 @@ public class VendaTest {
         produto.setPreco(30.0);
 
         venda.atualizaValorTotalDaVenda(produto,2);
+        Assertions.assertEquals(60.0, venda.getValorTotalVendido());
+    }
+
+    @Test
+    public void deveSomarVendaDeVariosProdutos(){
+        Produto produto1 = new Produto();
+        Produto produto2 = new Produto();
+        Venda venda = new Venda();
+
+        produto1.setPreco(10.0);
+        produto2.setPreco(20.0);
+
+
+        venda.atualizaValorTotalDaVenda(produto1,2);
+        Assertions.assertEquals(20.0, venda.getValorTotalVendido());
+        venda.atualizaValorTotalDaVenda(produto2,2);
         Assertions.assertEquals(60.0, venda.getValorTotalVendido());
     }
 }
